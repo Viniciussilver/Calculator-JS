085604087d6423861c2b2a41127b568c74082d37
Viniciussilver <viniciussilverk@gmail.com>
2022-08-16 18:43:44 -0300

    updateTwo

--- calc/index.js
+++ calc/index.js
@@ -1,39 +1,23 @@
+const number = document.getElementById("result");
 
-const number= document.getElementById("result")
-
-function insert(num){
-     number.innerHTML= number.innerHTML + num
-
-} 
-
-function clean(){
-    number.innerHTML= ""
-}
-function back(){
-    const result = number.innerHTML
-    number.innerHTML= result.substring(0,result.length -1)
+function insert(num) {
+  number.innerHTML = number.innerHTML + num;
 }
 
-function calculate(){
-        if(number.innerHTML){
-        return number.innerHTML= eval(number.innerHTML)
-    }
+function clean() {
+  number.innerHTML = '';
+}
+function back() {
+  const result = number.innerHTML;
+  number.innerHTML = result.substring(0, result.length - 1);
 }
 
-const hours= document.getElementById("static")
+function calculate() {
+  if (number.innerHTML) {
+    return (number.innerHTML = eval(number.innerHTML));
+  }
+}
 
-const Tempo=()=>{
-    let date= new Date();
-    let hr= date.getHours();
-    let min= date.getMinutes();
-    let ss= date.getSeconds();
 
-    hr = hr <10 ? `0${hr}` : hr;
-    min = min <10 ? `0${min}` : min;
-    ss = ss <10 ? `0${ss}` : ss;
 
-    let time= `${hr}:${min}:${ss}`
-    hours.innerHTML= time
-};
 
-Tempo(setInterval(Tempo,1000));
